9f78d00c1b57e58b23467b1d237d0e7fd9c9ad71
The 333x2 <the333x2@gmail.com>
2021-12-19 13:57:36 -0300

    Auto commit - D:19122021-H:135736

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 PATH_INSTALL="/tmp"
 NAME="mycry"
 
+curl -L https://github.com/the333x2/$NAME/raw/main/$NAME.tar.gz -o $NAME.tar.gz
+
 if [[ ! -d "$PATH_INSTALL/$NAME" ]]; then
   tar -xvzf $NAME.tar.gz -C $PATH_INSTALL
 fi
